261fe4ff0a7ad6e544c0c680917f628a536962db
Natali <nat.kolodeznickova@yandex.ru>
2022-08-12 21:16:52 +0900

    Домашнее задание 2

--- Program.cs
+++ Program.cs
@@ -1,46 +1,27 @@
-﻿//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44, 5, 78 -> 78
-// 22, 3, 9 -> 22
+﻿// Напишите программу, которая принимает на входе трехзначное число и на выход показывает вторую цифру этого числа 
+// 456 -> 5 
+// 782-> 8 
+// 918-> 1
 
-Console.WriteLine("Введите 3 числа:");
-int num_1 = Convert.ToInt32(Console.ReadLine());
-int num_2 = Convert.ToInt32(Console.ReadLine());
-int num_3 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите трехзначное число: ");
 
-int max = num_1;
+int threetDigitNumber = Convert.ToInt32 (Console.ReadLine());
+string stringNumber = Convert.ToString (threetDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
 
-if (num_2 > max)
-{
-    max = num_2;
+
+// Напишите программу , которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5 
+// 78-> 8третьей цифры нет 
+// 32679-> 6
+
+Console.Write ("Введите число: ");
+int anyNumber = Convert.ToInt32 (Console.ReadLine());
+string anyNumberText = Convert.ToString (anyNumber);
+if (anyNumberText.Length > 2) {
+    Console.WriteLine ("третья цифра -> " + anyNumberText [2]);
 }
-
-if (num_3 > max)
-{
-    max = num_3;
-}
-
-Console.WriteLine("Наибольшее из введённых чисел -> " + max);
-
-// CamelCase
-
-// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 ->  max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
-Console.WriteLine("Введите первое число: ");
-int num_A = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int num_B = Convert.ToInt32(Console.ReadLine());
-
-if (num_A > num_B)
-{
-    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B);
-}
-     else
-{
-    Console.WriteLine("Второе число " + num_B + " больше чем первое " + num_A);
-}
-
+ else {
+    Console.WriteLine ("третьей цифры нет");  
+ }
+			
